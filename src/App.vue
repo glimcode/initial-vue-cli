caef6df14e5ee67b433db314765ed7a73bea9ced
limcgz <lmczyxq@163.com>
2019-08-01 09:23:02 +0800

    feat: 引入stylus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import './assets/stylus/index.styl'
 export default {
   name: 'App'
 }
